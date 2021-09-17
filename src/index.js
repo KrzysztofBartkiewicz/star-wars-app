800c2395fdadc565f6b834666d87f8b33bd71b29
Krzysztof Bartkiewicz <bartkiewicz.krzysztof84@gmail.com>
2021-09-17 07:48:57 +0200

    add main theme

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
 import App from './App';
 import GlobalStyle from './styles/global';
+import mainTheme from './theme/main';
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+    <ThemeProvider theme={mainTheme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/theme/main.js
@@ -0,0 +1,13 @@
+const mainTheme = {
+  colors: {},
+
+  fontSizes: {},
+
+  fontWeights: {
+    regular: 400,
+    semiBold: 600,
+    bold: 700,
+  },
+};
+
+export default mainTheme;
