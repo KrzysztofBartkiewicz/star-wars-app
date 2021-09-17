115b383b49f577314c5b2abfb351579c52181c10
Krzysztof Bartkiewicz <bartkiewicz.krzysztof84@gmail.com>
2021-09-17 12:06:27 +0200

    add home & characters views

---
+++ src/views/Characters/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { StyledCharacters } from './StyledCharacters';
+
+const Characters = () => {
+  return <StyledCharacters>Characters</StyledCharacters>;
+};
+
+export default Characters;
---
+++ src/views/Home/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { StyledHome } from './StyledHome';
+
+const Home = () => {
+  return <StyledHome>Home</StyledHome>;
+};
+
+export default Home;
