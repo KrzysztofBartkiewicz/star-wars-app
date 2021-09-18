e8c9ef322b4dbbd069f3afabd3f4992580ec692a
Krzysztof Bartkiewicz <bartkiewicz.krzysztof84@gmail.com>
2021-09-18 12:46:21 +0200

    add single character info

--- src/views/SingleCharacter/StyleSingleCharacter.js
+++ src/views/SingleCharacter/StyleSingleCharacter.js
@@ -1,3 +1,8 @@
 import styled from 'styled-components';
+import bg from '../../assets/images/bg3.jpg';
 
-export const StyledSingleCharacter = styled.section``;
+export const StyledSingleCharacter = styled.section`
+  ${({ theme }) => theme.mixins.view(bg)}
+  display: flex;
+  justify-content: space-around;
+`;
--- src/views/SingleCharacter/index.js
+++ src/views/SingleCharacter/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import CharacterDetails from '../../components/organisms/CharacterDetails';
 import { StyledSingleCharacter } from './StyleSingleCharacter';
 
-const SingleCharacter = () => {
-  return <StyledSingleCharacter></StyledSingleCharacter>;
+const SingleCharacter = ({ location }) => {
+  return (
+    <StyledSingleCharacter>
+      <CharacterDetails details={location.state} />
+    </StyledSingleCharacter>
+  );
 };
 
 export default SingleCharacter;
