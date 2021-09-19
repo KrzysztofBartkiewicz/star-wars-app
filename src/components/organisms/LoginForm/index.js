9935646ea6acd55daa27288dfa0aa79f3b4859f0
Krzysztof Bartkiewicz <bartkiewicz.krzysztof84@gmail.com>
2021-09-19 23:49:13 +0200

    add validattion to login form

--- src/components/organisms/LoginForm/index.js
+++ src/components/organisms/LoginForm/index.js
@@ -1,12 +1,23 @@
 import React, { useContext } from 'react';
 import { useFormik } from 'formik';
 import FormInput from '../../molecules/FormInput';
+import Heading from '../../atoms/Heading';
 import Button from '../../atoms/Button';
 import { AuthContext } from '../../../context';
 import { StyledLoginForm } from './StyledLoginForm';
+import * as yup from 'yup';
+import { getUserData } from '../../../redux/userReducer/selectors';
+import { useSelector } from 'react-redux';
+import { formatError } from '../../../helpers';
+
+const schema = yup.object().shape({
+  login: yup.string().required("Login can't be empty"),
+  password: yup.string().required("Password can't be empty"),
+});
 
 const LoginForm = () => {
   const { signIn } = useContext(AuthContext);
+  const { error } = useSelector(getUserData);
 
   const formik = useFormik({
     initialValues: {
@@ -18,10 +29,12 @@ const LoginForm = () => {
       signIn(email, password);
       resetForm();
     },
+    validationSchema: schema,
   });
 
   return (
     <StyledLoginForm onSubmit={formik.handleSubmit}>
+      {error && <Heading color="red">{formatError(error.code)}</Heading>}
       <FormInput
         label="Login"
         name="login"
