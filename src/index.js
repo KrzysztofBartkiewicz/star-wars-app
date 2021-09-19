22e732028d95ff0528420c45632b313f76b902c3
Krzysztof Bartkiewicz <bartkiewicz.krzysztof84@gmail.com>
2021-09-19 23:53:13 +0200

    add auth context provider

--- src/index.js
+++ src/index.js
@@ -6,14 +6,17 @@ import store from './redux/store';
 import App from './App';
 import GlobalStyle from './styles/global';
 import mainTheme from './theme/main';
+import AuthContextProvider from './providers/AuthContextProvider';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <ThemeProvider theme={mainTheme}>
-        <GlobalStyle />
-        <App />
-      </ThemeProvider>
+      <AuthContextProvider>
+        <ThemeProvider theme={mainTheme}>
+          <GlobalStyle />
+          <App />
+        </ThemeProvider>
+      </AuthContextProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
