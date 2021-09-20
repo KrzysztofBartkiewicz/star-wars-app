09dc52602fdd9a96d3cb816d78e2febfecce827b
Krzysztof Bartkiewicz <bartkiewicz.krzysztof84@gmail.com>
2021-09-20 18:01:32 +0200

    remove unused imports

--- src/components/organisms/CharacterDetails/index.js
+++ src/components/organisms/CharacterDetails/index.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 import Heading from '../../atoms/Heading';
 import Paragraph from '../../atoms/Paragraph';
 import { useDispatch, useSelector } from 'react-redux';
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -13,4 +13,3 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 export const auth = firebase.auth();
-export const provider = new firebase.auth.GoogleAuthProvider();
